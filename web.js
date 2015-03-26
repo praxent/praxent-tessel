33e02cf815c316fe6afbe32a9a379e02eb3c3081
Josh Gillespie <josh@astonishdesign.com>
2015-03-26 13:17:34 -0500

    adds index.html
    Signed-off-by: Josh Gillespie <josh@astonishdesign.com>

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
