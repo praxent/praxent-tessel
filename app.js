eaa5a3b2cde6cc2d59020276ed0067ce48c5658e
Josh Gillespie <josh@astonishdesign.com>
2015-03-26 11:59:53 -0500

    adds app.listen to express
    Signed-off-by: Josh Gillespie <josh@astonishdesign.com>

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.listen(3000);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
