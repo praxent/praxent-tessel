e60c4bec4e68288273d9283d242fe0a9d5a2a710
Josh Gillespie <josh@astonishdesign.com>
2015-03-26 10:47:31 -0500

    Swaps out camera-S3 module for AWS module
    - Refactors takePicture function to use AWS

--- camera.js
+++ camera.js
@@ -10,13 +10,22 @@ tessel run camera.js
 *********************************************/
 
 var tessel = require('tessel');
-var camera = require('camera-vc0706').use(tessel.port['A']);
-var uploadImage = require('tessel-camera-s3');
+var camera = require('camera-vc0706').use(tessel.port.A);
+var AWS = require('aws-sdk');
+var config = {
+      accessKeyId: 'AKIAJ3IDS2XTRQXZWRZQ',
+      secretAccessKey: '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',
+    };
+
+AWS.config.update(config);
 
 var notificationLED = tessel.led[3]; // Set up an LED to notify when we're taking a picture
 
 // Wait for the camera module to say it's ready
 camera.on('ready', function() {
+
+  // Name the image
+  var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
 
   console.log('READY');
 
@@ -28,28 +37,25 @@ camera.on('ready', function() {
       return console.log('error taking image', err);
     }
 
-    var s3Config = {
-      key:'AKIAJ3IDS2XTRQXZWRZQ',
-      secret:'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',
-      bucket:'praxent'
-    };
+    var s3 = new AWS.S3(),
+        params = {
+          Bucket: 'praxent',
+          Key: name,
+          Body: image
+        };
 
-    notificationLED.low();
+    s3.putObject(params, function(err, data) {
 
-    // Name the image
-    var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
-
-    uploadImage(image, name, s3Config, function(err, res) {
       if (err) {
-        return console.log('There was an error :( ');
+        return console.log('ERROR ', err);
       }
 
-      console.log('image was successfully uploaded!');
-      console.log('RESPONSE -=-=-=-=- ', res);
+      console.log("Successfully uploaded data to myBucket/myKey");
 
-      // Turn the camera off to end the script
+      notificationLED.low();
       camera.disable();
-    });
+
+     });
 
   });
 });
