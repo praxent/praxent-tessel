b25a78aee45e2a4b43f5f14a748da1815429d56c
Josh Gillespie <josh@astonishdesign.com>
2015-03-26 15:47:04 -0500

    adds current climate to view
    Signed-off-by: Josh Gillespie <josh@astonishdesign.com>

--- public/app.js
+++ public/app.js
@@ -2,12 +2,27 @@
 $(document).ready(function() {
 
     setInterval(function() {
-
-        var d = new Date();
-        console.log('refreshing ', d.getTime());
-
-        $(".live-image").attr("src", "https://s3.amazonaws.com/praxent/officeCurrent.jpg?"+d.getTime());
-
+        resetImage();
     }, 5000);
+
+    $.get("/climate", function (data) {
+        var climate = JSON.parse(data);
+
+        $('.temp').html(climate.temp);
+        $('.humidity').html(climate.humidity);
+
+        console.log('temp ', climate.temp);
+        console.log('humidity ', climate.humidity);
+    });
+
+    $(document).on('click', '.refresh', function() {
+        console.log('resetting');
+        resetImage();
+    });
+
+    function resetImage() {
+        var d = new Date();
+        $(".live-image").attr("src", "https://s3.amazonaws.com/praxent/officeCurrent.jpg?"+d.getTime());
+    }
 
 });
